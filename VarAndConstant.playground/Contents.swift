49949ae51223e163857a694784a873727caeb87d
sam <twinsamson@gmail.com>
2019-04-16 11:28:08 +0700

    Variable and Constant

---
+++ VarAndConstant.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+//Explicit Variable
+var myName = "sammy"
+var myNumber = 123456789
+var myNumber2 = 3.1415926
+var status = true
+
+//Implicit Varialble
+
+var mySurname:String = "dwdugdu" //Srting Type
+var myNumber3:Int = Int(555.5)
+var myNumber4:Double = 12.34567
+var status2:Bool = false
+
+
+    
